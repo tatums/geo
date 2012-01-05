01ed93b6d31b9e24b4f58a32e06f8386cb6f480a
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-01-04 21:40:10 -0600

    fixins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PrettyQuick::Application.routes.draw do
+  get "location/index"
+
   resources :tasks
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ PrettyQuick::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'tasks#index'
+   root :to => 'location#index'
 
   # See how all your routes lay out with "rake routes"
 
