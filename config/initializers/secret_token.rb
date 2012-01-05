c1854ddf8a4e72ed7ab181f6bcd5c635c0aa4d90
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-01-04 21:31:24 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrettyQuick::Application.config.secret_token = '035c46aa1e6024896dc6c20044813f3472c90fe33a51e95293b9303d2ff1c46befc5559694e40ceaad021bc21507b078e3fb7e718d2b2677d81239e20df31a04'
